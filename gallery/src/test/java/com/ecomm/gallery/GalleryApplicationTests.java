b0c498d760564947fc3e26622cc72f925c494742
faisalhardin <faisalhardin23@gmail.com>
2020-05-31 16:43:23 +0700

    init

---
+++ gallery/src/test/java/com/ecomm/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
